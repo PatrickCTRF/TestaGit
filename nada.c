983dd98e2e9abae038d750f3515f76f189f9d101
Patrick Ferreira <patrickctrf@hotmail.com.br>
2017-08-03 19:29:56 -0300

    Escrevi mais um pouco

--- nada.c
+++ nada.c
@@ -1,9 +1,27 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+int trataString(char *);//A função que receberá a string inserida e retornará a quantidade de palavras na mesma.
+
 int main(){
+  int QtdPalavras = 0;
+  char *str = null;//O ponteiro para array de caracteres que guardará nossa string.
 
-  char *str = null
+  scanf("%[^\n]", str);//Recebemos a string de entrada até ser inserido o caractere de uebra de linha.
+  
+  if(str != null) resultado = trataString(str);//Somente chama a função se a string str não estiver VAZIA (quando o usuário não insere nenhuma palavra e clica enter).
+  
+  printf("Palavras: %d\n", resultado);
+}
 
-  scanf("%[^\n]", str);
+int trataString(char *str){
+ int i = 0;
+  int QtdPalavras = 0;
+  
+  for(i = 0; str[i] != '\0'; i++){
+   
+    
+    
+  }
+  
 }
